caa19b3fc36170e40c5ba259736b87f2a463bb1c
David Aguilar <gd.aguilarc@gmail.com>
2019-04-09 12:37:17 -0500

    Upload page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import multer from 'multer';
+
+const upload = multer({ dest: 'uploads/' });
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('layout');
+  res.render('index');
 });
+
+router.post('/', upload.single('file'), (req, res) => {});
 
 export default router;
