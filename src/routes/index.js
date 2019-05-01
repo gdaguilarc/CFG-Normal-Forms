0df36fef0a7c05a78bffaf20ba5a5ff487d5f504
David Aguilar <gd.aguilarc@gmail.com>
2019-05-01 16:13:05 -0500

    Other changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import multer from 'multer';
 
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: './uploads/' });
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('index');
 });
 
-router.post('/', upload.single('file'), (req, res) => {});
+router.post('/', upload.single('glc'), (req, res) => {
+  console.log(req.file);
+});
 
 export default router;
