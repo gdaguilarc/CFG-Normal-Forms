99979482668f9520b3d73d910b913c03f85b9c7d
David Aguilar <gd.aguilarc@gmail.com>
2019-05-04 18:28:59 -0500

    Basic index page and upload function

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 // lib/app.ts
-import express = require('express');
+import express from 'express';
+import path from 'path';
+import routes from './routes/index';
 
 // Create a new express application instance
 const app: express.Application = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.use(routes);
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
