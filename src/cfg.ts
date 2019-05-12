a0367540603db536921d98e22e1906cb25ca37c2
Saul Neri <saul.nerio.1998@gmail.com>
2019-05-12 14:46:21 -0500

    chain rules, uselles symbols
    implemented algorithms for chain rules and useless symbols.
    Replaced ' to * for prime variables.
    Added test4.txt and test5.txt.
    Strated implemetation for chimsky, the advanced length method calculates
    taking into account prime and T1, T2, Tn... variables.

--- src/cfg.ts
+++ src/cfg.ts
@@ -33,7 +33,48 @@ class CFG {
    * chomsky
    */
   public chomsky() {
-    this.eliminateLambdaRules();
+    // this.nonRecursiveInitial();
+    // this.eliminateLambdaRules();
+    // this.chainRules();
+    // this.uselessSymbols();
+
+    const advancedLength = (rule) => {
+      const regex1 = RegExp('[A-Z]\\d+', 'g');
+      const regex2 = RegExp('[A-Z]\\*', 'g');
+
+      const array1 = [];
+      const array2 = [];
+
+      let a = [];
+      while ((a = regex1.exec(rule)) !== null) {
+        array1.push(a[0]);
+      }
+
+      let a2 = [];
+      while ((a2 = regex2.exec(rule)) !== null) {
+        array2.push(a2[0]);
+      }
+
+      let len = rule.length;
+      len = len - array2.length;
+      let lenNum = 0;
+      array1.forEach((elem) => {
+        lenNum = lenNum + elem.length - 1; 
+      });
+      len = len - lenNum;
+
+      return len;
+    };
+    let newRules = new Map<string, string[]>();
+    for (const key of this.rules.keys()) {
+      for (const rule of this.getRule(key)) {
+        const len = advancedLength(rule);
+        if (len > 2) {
+
+        }
+      }
+    }
+
   }
 
   /**
@@ -61,7 +102,7 @@ class CFG {
       const letters = elem.split('');
       letters.forEach(letter => {
         if (letter === i) {
-          this.rules.set(i + "'", i);
+          this.rules.set(i + "*", i);
           return;
         }
       });
@@ -150,6 +191,203 @@ class CFG {
       this.rules.set(nul[nul.length - 1], newStates);
       console.log('NEW', this.rules);
     }
+  }
+
+  private chainRules() {
+    let chains = new Map<string, string[]>();
+    const equalSets = (setA, setB) => {
+      if (setA.length === setB.length) {
+        return setA.every((value, index) => {
+          return value === setB[index]; 
+        });
+      } else {
+        return false;
+      }
+    };
+    const substract = (setA, setB) => {
+      return setA.filter((elem) => {
+        return !setB.includes(elem);
+      });
+    }
+    for (const key of this.rules.keys()) {
+      let chainSet = [];
+      let prev = [];
+      let newSet = [];
+      chainSet.push(key);
+
+      while (!equalSets(chainSet, prev)) {
+        newSet = substract(chainSet, prev);
+        prev = [...chainSet];
+        for (const variable of newSet) {
+          const localRules = this.getRule(variable);
+          for (const localRule of localRules) {
+            if (localRule.length === 1 && this.isUpperCase(localRule)) {
+              if (!chainSet.includes(localRule)) {
+                chainSet.push(localRule);
+              }
+            }
+          }
+        }
+      }
+      chains.set(key, chainSet);
+    }
+    console.log("Chain sets", chains);
+
+    const unique = (value, index, self) => {
+      return self.indexOf(value) === index;
+    };
+    for (const mainKey of chains.keys()) {
+      const secondaryKeys = chains.get(mainKey);
+      let mainRules = this.getRule(mainKey);
+      for (const secondaryKey of secondaryKeys) {
+        if (secondaryKey !== mainKey) {
+          const secondaryRules = this.getRule(secondaryKey);
+          for (const secondaryRule of secondaryRules) {
+            if (secondaryRule !== secondaryKey) {
+              mainRules.push(secondaryRule);
+            }
+          }
+        }
+        while (mainRules.includes(secondaryKey)) {
+          const remIndex = mainRules.indexOf(secondaryKey);
+          if (remIndex !== -1) mainRules.splice(remIndex, 1);
+        }
+      }
+      while (mainRules.includes(mainKey)) {
+        const remIndex = mainRules.indexOf(mainKey);
+        if (remIndex !== -1) mainRules.splice(remIndex, 1);
+      }
+      mainRules = mainRules.filter(unique);
+      this.rules.set(mainKey, mainRules);
+
+    }
+    console.log('After chain rules', this.rules);
+  }
+
+  private uselessSymbols() {
+    let term = [];
+
+    for (const key of this.rules.keys()) {
+      const rules = this.getRule(key);
+      for (const rule of rules) {
+        let onlyTerminals = true;
+        for (const elem of rule.split("")) {
+          if (this.isUpperCase(elem)) {
+            onlyTerminals = false;
+            break;
+          }
+        }
+        if (onlyTerminals) {
+          if (!term.includes(key)) {
+            term.push(key);
+          }
+          break;
+        }
+      }
+    } 
+    const equalSets = (setA, setB) => {
+      if (setA.length === setB.length) {
+        return setA.every((value, index) => {
+          return value === setB[index]; 
+        });
+      } else {
+        return false;
+      }
+    };
+    const isOnlyPrevAndTerminal = ((rule, prevSet) => {
+      let isValid = true;
+      for (const element of rule.split("")) {
+        if (this.isUpperCase(element)) {
+          if (!prevSet.includes(element)) {
+            isValid = false;
+            break;
+          }
+        }
+      }
+      return isValid;
+    });
+    let prev = [];
+    while (!equalSets(term, prev)) {
+      prev = [...term];
+      for (const variable of this.rules.keys()) {
+        const rules = this.getRule(variable);
+        for (const rule of rules) {
+          if (isOnlyPrevAndTerminal(rule, prev)) {
+            if (!term.includes(variable)) {
+              term.push(variable)
+            }
+          }
+        }
+      }
+    }
+    console.log("Variables that lead to terminal", term);
+
+    let allVariables = this.rules.keys();
+    let variablesToRemove = [];
+    for (const variable of allVariables) {
+      if (!term.includes(variable)) {
+        this.rules.delete(variable);
+        variablesToRemove.push(variable)
+      }
+    }
+    for (const variable of this.rules.keys()) {
+      let rules = this.getRule(variable);
+      for (const rule of rules) {
+        for (const variableToRemove of variablesToRemove) {
+          if (rule.includes(variableToRemove)) {
+            const remIndex = rules.indexOf(rule);
+            if (remIndex !== -1) rules.splice(remIndex, 1);
+          }
+        }
+      }
+    }
+    console.log("After removing variables that don't lead to terminal", this.rules);
+
+    const substract = (setA, setB) => {
+      return setA.filter((elem) => {
+        return !setB.includes(elem);
+      });
+    }
+    let reach = [this.rules.keys().next().value];
+    let prevSet = []
+    let newSet = []
+    while (!equalSets(reach, prevSet)) {
+      newSet = substract(reach, prevSet);
+      prevSet = [...reach];
+      for (const variable of newSet) {
+        const rules = this.getRule(variable);
+        for (const rule of rules) {
+          for (const elem of rule.split("")) {
+            if (this.isUpperCase(elem)) {
+              if (!reach.includes(elem)) {
+                reach.push(elem);
+              }
+            }
+          }
+        }
+      }
+    }
+    console.log("Variables derivable from initial", reach);
+    allVariables = this.rules.keys();
+    variablesToRemove = [];
+    for (const variable of allVariables) {
+      if (!reach.includes(variable)) {
+        this.rules.delete(variable);
+        variablesToRemove.push(variable)
+      }
+    }
+    for (const variable of this.rules.keys()) {
+      let rules = this.getRule(variable);
+      for (const rule of rules) {
+        for (const variableToRemove of variablesToRemove) {
+          if (rule.includes(variableToRemove)) {
+            const remIndex = rules.indexOf(rule);
+            if (remIndex !== -1) rules.splice(remIndex, 1);
+          }
+        }
+      }
+    }
+    console.log("After removing variables that don't derive from initial", this.rules);
   }
 }
 
